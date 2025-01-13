0590827858fb157acce6ba629345ba0fff539781
Abhishek Nigam <mnigam925@gmail.com>
2025-01-13 21:02:24 +0530

    Initial Commit

---
+++ Strings/StringMethods.py
@@ -0,0 +1,10 @@
+#Different way of declaring the strings :
+s1 = "My Name is Priya"
+s2 = "My Age is 25"
+s3 = """We Are Learning
+ Strings"""
+s4 = '''Python is Dynamically Typed Language'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
---
+++ Strings/UserInput.py
@@ -0,0 +1,3 @@
+num1 = input('Enter the num1')
+print('Value of num1 is:',num1, 'Data Type of num1 is:',type(num1))
+
